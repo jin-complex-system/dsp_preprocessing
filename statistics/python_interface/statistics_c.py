b13eeb4327463b73e54c3883baab57cbad4a6442
Choong Jin Ng <jin@complexsysteminc.com>
2025-04-01 17:49:06 -0600

    Started implementation of statistics_c Python interface

--- statistics/python_interface/statistics_c.py
+++ statistics/python_interface/statistics_c.py
@@ -1,7 +1,20 @@
 import ctypes
+import numpy as np
+
+class Struct_StatisticsC(ctypes.Structure):
+    _fields_ = [
+        ("max", ctypes.c_float),
+        ("min", ctypes.c_float),
+        ("mean", ctypes.c_float),
+        ("median", ctypes.c_float),
+        ("variance", ctypes.c_float),
+        # ("first_der_mean", ctypes.c_float),
+        # ("first_der_variance", ctypes.c_float),
+    ]
 
 class statistics_c:
     libstatistics = None
+
     def __init__(
             self,
             library_path=""):
@@ -28,7 +41,7 @@ class statistics_c:
             frame_difference_reciprocal_float,
     ):
         """
-        Compute statistics from input_buffer within one frame
+        Compute statistics from input_buffer across different frames
 
         :param input_list_float:
         :param num_frames_uint32:
@@ -44,19 +57,51 @@ class statistics_c:
     def compute_statistics_within_frame(
             self,
             input_list_float,
-            bin_length_uint32,
+            num_frames_uint32,
             frame_difference_reciprocal_float,
     ):
         """
-        Compute statistics from input_buffer across different frames
+        Compute statistics from input_buffer within one frame
 
         :param input_list_float:
-        :param bin_length_uint32:
+        :param num_frames_uint32:
         :param frame_difference_reciprocal_float: non-zero, positive difference between each bin as a reciprocal
-        :return:
+        :return: float list of statistics across each frame
         """
+        # Check parameters
+        assert(self.libstatistics is not None)
+        assert(frame_difference_reciprocal_float > 0)
+        assert(num_frames_uint32 <= len(input_list_float))
 
-        # TODO: Implement this function
+        # Get the return types and argument types
+        self.libstatistics.compute_statistics_within_frame.restype = None
+        self.libstatistics.compute_statistics_within_frame.argtypes = [
+            np.ctypeslib.ndpointer(dtype=np.float32, ndim=1, flags='C_CONTIGUOUS'),
+            ctypes.c_uint32,
+            ctypes.c_float,
+            np.ctypeslib.ndpointer(dtype=np.float32, ndim=1, flags='C_CONTIGUOUS'),
+            ctypes.c_uint32,
+            np.ctypeslib.ndpointer(dtype=np.float32, ndim=1, flags='C_CONTIGUOUS'),
+            ctypes.c_uint32,
+        ]
 
-        return
+        # Prepare buffers
+        NUM_STATISTICS = ctypes.sizeof(Struct_StatisticsC)
 
+        output_buffer_length = int(NUM_STATISTICS * num_frames_uint32)
+        output_buffer = np.zeros(shape=[output_buffer_length], dtype=np.float32)
+
+        scratch_buffer_length = 2 * output_buffer_length
+        scratch_buffer = np.zeros(shape=[scratch_buffer_length], dtype=np.float32)
+
+        # Run function
+        self.libstatistics.compute_statistics_within_frame(
+            input_list_float,
+            ctypes.c_uint32(num_frames_uint32),
+            ctypes.c_float(frame_difference_reciprocal_float),
+            output_buffer,
+            ctypes.c_uint32(output_buffer_length),
+            scratch_buffer,
+            ctypes.c_uint32(scratch_buffer_length)
+        )
+        return output_buffer
--- statistics/python_unit_test/test_statistics.py
+++ statistics/python_unit_test/test_statistics.py
@@ -2,10 +2,13 @@ import unittest
 from python_interface.statistics_c import statistics_c
 import sys
 import glob
+import numpy as np
 
 CURRENT_BINARY_LOCATION = sys.argv[2]
 LIBRARY_PATH = glob.glob(CURRENT_BINARY_LOCATION + "/*_shared.dll")[0]
 stats_lib = statistics_c(library_path=LIBRARY_PATH)
+
+FLOAT_ERROR_NUM_PLACES = 5
 
 class PythonTestUtils(unittest.TestCase):
     def test_statistics_across_frames(self):
@@ -20,6 +23,16 @@ class PythonTestUtils(unittest.TestCase):
         # TODO: Implement unit test
 
         self.assertEqual(result_list, None)
+
+    def test_compute_statistics_within_frame(self):
+        input_list = np.array([0.0, 0.0, 1.0, 0.0, 0.0], dtype=np.float32)
+
+        result_list = stats_lib.compute_statistics_within_frame(
+            input_list,
+            len(input_list),
+            float(1.0),
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
