7e48a3c747da6c7d2fb95f5f77eab0c803de9b2a
Choong Jin Ng <jin@complexsysteminc.com>
2025-04-04 13:35:38 -0600

    Move unit test into mel_spectrogram unit test. Add unit test placeholders

--- audio_dsp/unit_test/test_mel_spectrogram.cpp
+++ audio_dsp/unit_test/test_mel_spectrogram.cpp
@@ -3,6 +3,7 @@
 #include <power_spectrum.h>
 
 #include <limits>
+#include <mel_spectrogram_unit_test/fft_unit_input.h>
 #include <mel_spectrogram_unit_test/mel_spectrogram_unit_test.h>
 #include <test_audio/audio_as_integer.h>
 #include <test_audio/power_spectrum.h>
@@ -18,6 +19,32 @@ MAX_AUDIO_DATA_TYPE = std::numeric_limits<audio_data_type>::max();
 
 constexpr double
 MEL_SPECTROGRAM_ERROR_TOLERANCE = 1e-3;
+
+TEST(MelSpectrogram, UnitFilterDistribution_32) {
+    /// Check parameters
+    {
+        assert(FFT_UNIT_INPUT_N_FFT > 0);
+        assert(MEL_SPECTROGRAM_N_MELS == PRECOMPUTED_MEL_SPECTROGRAM_N_MELS);
+        assert(FFT_UNIT_INPUT_NUM_FRAMES == MEL_SPECTROGRAM_NUM_FRAMES && FFT_UNIT_INPUT_NUM_FRAMES == 1);
+    }
+
+    float mel_spectrogram_buffer[MEL_SPECTROGRAM_UNIT_TEST_BUFFER_LENGTH] = {};
+
+    /// Compute mel spectrogram
+    compute_power_spectrum_into_mel_spectrogram(
+        FFT_UNIT_INPUT_BUFFER,
+        FFT_UNIT_INPUT_BUFFER_LENGTH,
+        mel_spectrogram_buffer,
+        MEL_SPECTROGRAM_UNIT_TEST_BUFFER_LENGTH);
+
+    /// Check against mel spectrogram
+    for (uint32_t mel_bin_iterator = 0; mel_bin_iterator < MEL_SPECTROGRAM_UNIT_TEST_BUFFER_LENGTH; mel_bin_iterator++) {
+        EXPECT_NEAR(
+            MEL_SPECTROGRAM_UNIT_TEST_BUFFER[mel_bin_iterator],
+            mel_spectrogram_buffer[mel_bin_iterator],
+            MEL_SPECTROGRAM_ERROR_TOLERANCE);
+    }
+}
 
 TEST(MelSpectrogram, PrecomputedMelSpectrogram)
 {
