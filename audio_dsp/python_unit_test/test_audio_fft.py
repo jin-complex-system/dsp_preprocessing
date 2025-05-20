c229f0e40dec6233504273ce38d52fb62d3a8f5c
Choong Jin Ng <jin@complexsysteminc.com>
2025-05-20 17:25:13 -0600

    Implement test to compare against librosa stft

--- audio_dsp/python_unit_test/test_audio_fft.py
+++ audio_dsp/python_unit_test/test_audio_fft.py
@@ -1,8 +1,11 @@
 import unittest
+
+import scipy.signal
 from python_interface.audio_dsp_c import audio_dsp_c
 import sys
 import glob
 import numpy as np
+import librosa
 
 CURRENT_BINARY_LOCATION = sys.argv[2]
 LIBRARY_PATH = glob.glob(CURRENT_BINARY_LOCATION + "/*_shared.dll")[0]
@@ -12,11 +15,75 @@ audio_dsp_c_lib = audio_dsp_c(library_path=LIBRARY_PATH)
 FFT_FLOAT64_ERROR_DELTA = 1e-4
 FFT_FLOAT32_ERROR_DELTA = FFT_FLOAT64_ERROR_DELTA
 
-class AudioDSP_FFT_PythonTestCase(unittest.TestCase):
-    def test_against_np_rfft(self):
-        n_fft_lengths = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+n_ffts = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 
-        for n_fft in n_fft_lengths:
+class AudioDSP_FFT_PythonTestCase(unittest.TestCase):
+    def test_against_librosa_stft(self):
+        for n_fft in n_ffts:
+            assert(n_fft % 2 == 0)
+            expected_output_complex_length = int(n_fft / 2 + 1)
+            expected_output_buffer_length = expected_output_complex_length * 2
+
+            # Prepare a random input 1D np.array()
+            random_input_float64 = np.random.uniform(
+                low=-1.0,
+                high=1.0,
+                size=n_fft,)
+            np.reshape(random_input_float64, shape=-1, copy=True)
+            assert (random_input_float64.shape[0] == n_fft and len(random_input_float64) == n_fft)
+            assert (len(random_input_float64.shape) == 1)
+            assert (random_input_float64.dtype == np.float64)
+
+            # Get outputs from numpy and library for float32
+            random_input_float32 = np.copy(random_input_float64, order='C').astype(np.float32)
+            assert (len(random_input_float32) == n_fft and random_input_float32.dtype == np.float32)
+            random_input_float32_with_hann = (
+                    random_input_float32 *
+                    scipy.signal.get_window(window="hann", Nx=n_fft, fftbins=True))
+
+            librosa_stft_result_float32 = librosa.stft(
+                y=random_input_float32,
+                n_fft=n_fft,
+                hop_length=n_fft,
+                win_length=n_fft,
+                center=False,
+            )[:, 0] # Only use the first frame
+            computed_result_float32 = audio_dsp_c_lib.perform_audio_dsp_rfft(
+                real_input_array=random_input_float32_with_hann,
+            )
+            self.assertEqual(len(librosa_stft_result_float32), expected_output_complex_length)
+            self.assertEqual(len(computed_result_float32), expected_output_buffer_length)
+
+            # Compare results
+            for iterator in range(0, expected_output_complex_length):
+                real_value_iterator = iterator * 2 + 0
+                img_value_iterator = iterator * 2 + 1
+
+                assert_fail_msg = "For n_fft = {}, FFT bin {} fail:\r\n ".format(n_fft, iterator)
+
+                # Compare against librosa for float32 input
+                self.assertAlmostEqual(
+                    first=np.real(librosa_stft_result_float32[iterator]),
+                    second=computed_result_float32[real_value_iterator],
+                    delta=FFT_FLOAT32_ERROR_DELTA,
+                    msg="{}real librosa float32 {} vs real lib float32 {}".format(
+                        assert_fail_msg,
+                        np.real(librosa_stft_result_float32[iterator]),
+                        computed_result_float32[real_value_iterator])
+                )
+                self.assertAlmostEqual(
+                    first=np.imag(librosa_stft_result_float32[iterator]),
+                    second=computed_result_float32[img_value_iterator],
+                    delta=FFT_FLOAT32_ERROR_DELTA,
+                    msg="{}img librosa float32 {} vs img lib float32 {}".format(
+                        assert_fail_msg,
+                        np.imag(librosa_stft_result_float32[iterator]),
+                        computed_result_float32[img_value_iterator])
+                )
+
+    def test_against_np_rfft(self):
+
+        for n_fft in n_ffts:
             assert(n_fft % 2 == 0)
             expected_output_complex_length = int(n_fft / 2 + 1)
             expected_output_buffer_length = expected_output_complex_length * 2
@@ -41,7 +108,7 @@ class AudioDSP_FFT_PythonTestCase(unittest.TestCase):
 
             # Get outputs from numpy and library for float32
             random_input_float32 = np.copy(random_input_float64, order='C').astype(np.float32)
-            assert(len(random_input_float32) == n_fft and random_input_float32.dtype == np.float32)
+            assert (len(random_input_float32) == n_fft and random_input_float32.dtype == np.float32)
 
             np_rfft_result_float32 = np.fft.rfft(random_input_float32)
             computed_result_float32 = audio_dsp_c_lib.perform_audio_dsp_rfft(
--- audio_dsp/python_unit_test/test_hann_window.py
+++ audio_dsp/python_unit_test/test_hann_window.py
@@ -1,5 +1,7 @@
 import unittest
+
 from python_interface.audio_dsp_c import audio_dsp_c
+import scipy.signal
 import sys
 import glob
 import math
@@ -9,12 +11,16 @@ CURRENT_BINARY_LOCATION = sys.argv[2]
 LIBRARY_PATH = glob.glob(CURRENT_BINARY_LOCATION + "/*_shared.dll")[0]
 audio_dsp_c_lib = audio_dsp_c(library_path=LIBRARY_PATH)
 
+lengths = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+lengths = [1024]
+scaling_factors = [1.0, 10.0, float(1.0 / np.iinfo(np.int16).max)]
+
 HANN_WINDOW_ERROR_DELTA = 1e-10
 
 
 def _get_hann_window_as_float(
         length,
-        scale_factor = 1.0,
+        scale_factor=1.0,
 ):
     """ Compute hanning window as a float for a given length
 
@@ -30,8 +36,8 @@ def _get_hann_window_as_float(
 
     hann_window_float = list()
 
-    for iterator in range(0,length):
-        hann_value =  0.5 - 0.5 * math.cos(2 * math.pi * iterator / (length - 1))
+    for iterator in range(0, length):
+        hann_value = 0.5 - 0.5 * math.cos(2 * math.pi * iterator / (length - 1))
         hann_value = hann_value * scale_factor
 
         assert(hann_value >= 0)
@@ -41,10 +47,58 @@ def _get_hann_window_as_float(
 
 
 class AudioDSP_HannWindow_PythonTestCase(unittest.TestCase):
-    def test_against_python_hann_window(self):
-        lengths = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
-        scaling_factors = [1.0, 10.0, float(1.0 / 32767.0)]
+    def test_against_scipy_hann_window(self):
+        for length in lengths:
+            for scaling_factor in scaling_factors:
+                assert(length > 1 and scaling_factor > 0.0)
 
+                assert_fail_prefix_msg = "Length: {}, scaling_factor: {}\r\n".format(
+                    length,
+                    scaling_factor
+                )
+
+                # Note that
+                # scipy.signal.get_window() result differs from scipy.signal.windows.hann()
+                scipy_result = scipy.signal.get_window(
+                    window="hann",
+                    Nx=length,
+                    fftbins=True)
+                scipy_result_scaled = (scipy_result * scaling_factor)
+                assert (len(scipy_result_scaled) == length)
+                hann_result = scipy.signal.windows.hann(
+                    M=length,
+                    sym=True
+                )
+                hann_result_scaled = (hann_result * scaling_factor)
+                assert (len(hann_result_scaled) == length)
+
+                computed_result = audio_dsp_c_lib.hann_window_compute(
+                    length_uint32=length,
+                    scaling_factor_float=scaling_factor,
+                )
+                self.assertEqual(len(computed_result), length)
+
+                for iterator in range(0, length):
+                    assert_fail_msg = "{} iterator mismatch at {}:\nscipy {}\nhann {}\ncomputed {}\n".format(
+                        assert_fail_prefix_msg,
+                        iterator,
+                        scipy_result_scaled,
+                        hann_result_scaled,
+                        computed_result
+                    )
+
+                    self.assertAlmostEqual(
+                        first=scipy_result_scaled[iterator],
+                        second=computed_result[iterator],
+                        delta=HANN_WINDOW_ERROR_DELTA,
+                        msg="{}".format(assert_fail_msg))
+                    self.assertAlmostEqual(
+                        first=hann_result_scaled[iterator],
+                        second=computed_result[iterator],
+                        delta=HANN_WINDOW_ERROR_DELTA,
+                        msg="{}".format(assert_fail_msg))
+
+    def test_against_python_hann_window(self):
         for length in lengths:
             for scaling_factor in scaling_factors:
                 assert(length > 1 and scaling_factor > 0.0)
@@ -56,7 +110,7 @@ class AudioDSP_HannWindow_PythonTestCase(unittest.TestCase):
                 python_result = np.array(_get_hann_window_as_float(
                     length=length,
                     scale_factor=scaling_factor), dtype=np.float32)
-                assert(len(python_result) == length)
+                assert (len(python_result) == length)
 
                 computed_result = audio_dsp_c_lib.hann_window_compute(
                     length_uint32=length,
