937459653531c3c231527501b78bad4b9c3d9695
Choong Jin Ng <jin@complexsysteminc.com>
2025-06-13 10:04:07 -0600

    Add ARM support for computing square roots

--- utils/src/square_root_approximation.c
+++ utils/src/square_root_approximation.c
@@ -12,8 +12,17 @@ square_root_approximation(
         return 0.0f;
     }
 
+#ifdef __ARM_ARCH
+    float output = 0.0f;
+
+    arm_status status =
+    arm_sqrt_f32(target_value, &output);
+    assert(status == ARM_MATH_SUCCESS);
+
+    return output;
+#else
     // TODO: Investigate faster way to perform square root
     // For now, just use math library
     return sqrtf(target_value);
-
+#endif // __ARM_ARCH
 }
