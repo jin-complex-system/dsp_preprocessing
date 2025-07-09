caebf9251047d12471793b7214f6aa57ce7745fd
Choong Jin Ng <jin@complexsysteminc.com>
2025-07-09 12:28:21 -0600

    Implement python interface and unit test for Scaler

--- scaler/python_interface/scaler_c.py
+++ scaler/python_interface/scaler_c.py
@@ -22,3 +22,129 @@ class scaler_c:
 
         assert (library_path is not None and len(library_path) > 0)
         self.libscaler = ctypes.CDLL(library_path)
+
+    def scale_float_buffer(
+            self,
+            input_buffer_float,
+            top_value_float,
+            bottom_value_float):
+        """
+        Scale input_buffer to [0.0, 1.0] as float. Clips between top_value and bottom_value
+
+        :param input_buffer_float:
+        :param top_value_float:
+        :param bottom_value_float:
+        :return:
+        """
+
+        # Check parameters
+        assert (self.libscaler is not None)
+        assert (isinstance(input_buffer_float, list) or isinstance(input_buffer_float, np.ndarray))
+        assert (len(input_buffer_float) > 0)
+        assert (isinstance(input_buffer_float[0], float) or isinstance(input_buffer_float[0], np.float32))
+        assert (isinstance(top_value_float, float) or isinstance(top_value_float, np.float32))
+        assert (isinstance(bottom_value_float, float) or isinstance(bottom_value_float, np.float32))
+        assert (top_value_float > bottom_value_float)
+
+        # Grab constants
+        num_elements = len(input_buffer_float)
+
+        # Prepare input buffer with a deep copy
+        input_buffer = np.reshape(
+            input_buffer_float,
+            shape=-1,
+            copy=True, ).astype(np.float32)
+        assert (len(input_buffer) == num_elements)
+        assert (len(input_buffer) == input_buffer.shape[0])
+        assert (input_buffer.dtype == np.float32)
+
+        # Prepare output buffer
+        output_buffer = np.zeros(shape=[num_elements], dtype=np.float32)
+        assert (len(output_buffer) == num_elements)
+        assert (len(output_buffer) == input_buffer.shape[0])
+        assert (output_buffer.dtype == np.float32)
+
+        # Set the return types and argument types
+        self.libscaler.scale_float_buffer.restype = None
+        self.libscaler.scale_float_buffer.argtypes = [
+            np.ctypeslib.ndpointer(
+                shape=[num_elements], dtype=np.float32, ndim=1),
+            np.ctypeslib.ndpointer(
+                shape=[num_elements], dtype=np.float32, ndim=1),
+            ctypes.c_uint32,
+            ctypes.c_float,
+            ctypes.c_float,
+        ]
+
+        self.libscaler.scale_float_buffer(
+            input_buffer,
+            output_buffer,
+            ctypes.c_uint32(num_elements),
+            ctypes.c_float(top_value_float),
+            ctypes.c_float(bottom_value_float),
+        )
+
+        return output_buffer
+
+    def scale_float_buffer_quantised(
+            self,
+            input_buffer_float,
+            top_value_float,
+            bottom_value_float):
+        """
+        Scale input_buffer to [0, 255] as uint8. Clips between top_value and bottom_value
+
+        :param input_buffer_float:
+        :param top_value_float:
+        :param bottom_value_float:
+        :return:
+        """
+
+        # Check parameters
+        assert (self.libscaler is not None)
+        assert (isinstance(input_buffer_float, list) or isinstance(input_buffer_float, np.ndarray))
+        assert (len(input_buffer_float) > 0)
+        assert (isinstance(input_buffer_float[0], float) or isinstance(input_buffer_float[0], np.float32))
+        assert (isinstance(top_value_float, float) or isinstance(top_value_float, np.float32))
+        assert (isinstance(bottom_value_float, float) or isinstance(bottom_value_float, np.float32))
+        assert (top_value_float > bottom_value_float)
+
+        # Grab constants
+        num_elements = len(input_buffer_float)
+
+        # Prepare input buffer with a deep copy
+        input_buffer = np.reshape(
+            input_buffer_float,
+            shape=-1,
+            copy=True, ).astype(np.float32)
+        assert (len(input_buffer) == num_elements)
+        assert (len(input_buffer) == input_buffer.shape[0])
+        assert (input_buffer.dtype == np.float32)
+
+        # Prepare output buffer
+        output_buffer = np.zeros(shape=[num_elements], dtype=np.uint8)
+        assert (len(output_buffer) == num_elements)
+        assert (len(output_buffer) == input_buffer.shape[0])
+        assert (output_buffer.dtype == np.uint8)
+
+        # Set the return types and argument types
+        self.libscaler.scale_float_buffer_quantised.restype = None
+        self.libscaler.scale_float_buffer_quantised.argtypes = [
+            np.ctypeslib.ndpointer(
+                shape=[num_elements], dtype=np.float32, ndim=1),
+            np.ctypeslib.ndpointer(
+                shape=[num_elements], dtype=np.uint8, ndim=1),
+            ctypes.c_uint32,
+            ctypes.c_float,
+            ctypes.c_float,
+        ]
+
+        self.libscaler.scale_float_buffer_quantised(
+            input_buffer,
+            output_buffer,
+            ctypes.c_uint32(num_elements),
+            ctypes.c_float(top_value_float),
+            ctypes.c_float(bottom_value_float),
+        )
+
+        return output_buffer
--- scaler/python_unit_test/test_scaler.py
+++ scaler/python_unit_test/test_scaler.py
@@ -10,11 +10,116 @@ CURRENT_BINARY_LOCATION = sys.argv[2]
 LIBRARY_PATH = glob.glob(CURRENT_BINARY_LOCATION + "/*_shared.dll")[0]
 scaler_c_lib = scaler_c(library_path=LIBRARY_PATH)
 
+
 class Scaler_PythonTestCase(unittest.TestCase):
     def test_scaler_float(self):
-        my_input = [0.0, 100.0]
+        ERROR_TOLERANCE_DELTA = 1e-7
+        ROUNDING_ERROR_TOLERANCE = 1.1
+        UINT8_MAX = np.iinfo(np.uint8).max
+        UINT8_MIN = np.iinfo(np.uint8).min
+
+        # Prepare input array, top_value and bottom_value
+
+        my_input_float_list = [0.0, -25.0, 1e-7, 5 * 1e-9, 2000, 100.0, -9999.999992]
+        my_input_float_np_array = np.array(my_input_float_list, dtype=np.float32)
+
+        np_min_my_input = np.min(my_input_float_np_array)
+        np_max_my_input = np.max(my_input_float_np_array)
+
+        min_values = [np_min_my_input, np_min_my_input + 200, np_min_my_input - 200, -80.0]
+        max_values = [np_max_my_input, np_max_my_input + 200, np_max_my_input - 200, 0.0]
+        assert (np.min(min_values) < np.max(max_values))
+
+        num_elements = len(my_input_float_list)
+
+        # Iterate through different min_values and max_values
+        for min_value in min_values:
+            for max_value in max_values:
+                value_range = max_value - min_value
+                expected_results_float = (my_input_float_np_array - min_value) / value_range
+                expected_results_float = np.clip(
+                    expected_results_float,
+                    a_min=0.0,
+                    a_max=1.0)
+                expected_results_uint8 = (expected_results_float * UINT8_MAX).astype(np.uint8)
+                assert (len(expected_results_float) == len(expected_results_uint8))
+
+                output_buffer_float_from_list = scaler_c_lib.scale_float_buffer(
+                    input_buffer_float=my_input_float_list,
+                    top_value_float=max_value,
+                    bottom_value_float=min_value)
+                output_buffer_float_from_np_array = scaler_c_lib.scale_float_buffer(
+                    input_buffer_float=my_input_float_np_array,
+                    top_value_float=max_value,
+                    bottom_value_float=min_value)
+
+                output_buffer_quantised_from_list = scaler_c_lib.scale_float_buffer_quantised(
+                    input_buffer_float=my_input_float_list,
+                    top_value_float=max_value,
+                    bottom_value_float=min_value)
+                output_buffer_quantised_from_np_array = scaler_c_lib.scale_float_buffer_quantised(
+                    input_buffer_float=my_input_float_np_array,
+                    top_value_float=max_value,
+                    bottom_value_float=min_value)
+
+                # Check lengths
+                self.assertEqual(
+                    len(output_buffer_float_from_list),
+                    num_elements
+                )
+                self.assertEqual(
+                    len(output_buffer_float_from_list),
+                    len(output_buffer_float_from_np_array)
+                )
+                self.assertEqual(
+                    len(output_buffer_quantised_from_list),
+                    len(output_buffer_quantised_from_np_array)
+                )
+                self.assertEqual(
+                    len(output_buffer_float_from_list),
+                    len(output_buffer_quantised_from_list)
+                )
+
+                # Check bounds for float
+                self.assertLessEqual(np.max(output_buffer_float_from_list), 1.0)
+                self.assertLessEqual(np.max(output_buffer_float_from_np_array), 1.0)
+                self.assertGreaterEqual(np.min(output_buffer_float_from_list), 0.0)
+                self.assertGreaterEqual(np.min(output_buffer_float_from_np_array), 0.0)
+
+                # Check bounds for uint8
+                self.assertLessEqual(np.max(output_buffer_quantised_from_list), UINT8_MAX)
+                self.assertLessEqual(np.max(output_buffer_quantised_from_np_array), UINT8_MAX)
+                self.assertGreaterEqual(np.min(output_buffer_quantised_from_list), UINT8_MIN)
+                self.assertGreaterEqual(np.min(output_buffer_quantised_from_np_array), UINT8_MIN)
+
+                # Check results
+                for element_iterator in (range(0, num_elements)):
+                    self.assertAlmostEqual(
+                        first=output_buffer_float_from_list[element_iterator],
+                        second=expected_results_float[element_iterator],
+                        delta=ERROR_TOLERANCE_DELTA,
+                    )
+                    self.assertAlmostEqual(
+                        first=output_buffer_float_from_np_array[element_iterator],
+                        second=expected_results_float[element_iterator],
+                        delta=ERROR_TOLERANCE_DELTA,
+                    )
+                    self.assertAlmostEqual(
+                        first=float(output_buffer_quantised_from_list[element_iterator]),
+                        second=float(expected_results_uint8[element_iterator]),
+                        delta=ROUNDING_ERROR_TOLERANCE,
+                    )
+                    self.assertAlmostEqual(
+                        first=float(output_buffer_quantised_from_np_array[element_iterator]),
+                        second=float(expected_results_uint8[element_iterator]),
+                        delta=ROUNDING_ERROR_TOLERANCE,
+                    )
+                    self.assertAlmostEqual(
+                        first=output_buffer_float_from_np_array[element_iterator] * UINT8_MAX,
+                        second=float(output_buffer_quantised_from_np_array[element_iterator].astype(np.float32)),
+                        delta=ROUNDING_ERROR_TOLERANCE,
+                    )
 
 
 if __name__ == '__main__':
     unittest.main()
-
